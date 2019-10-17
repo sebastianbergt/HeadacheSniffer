3471848a3cc885f8bdb833f45167e61ff79788bf
Sebastian Bergt <sebastian.bergt@aid-driving.eu>
2019-10-17 10:25:23 +0200

    ignoring the warmup time will give good enough results after a few hours

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,45 @@
 #include <Arduino.h>
+#include "MHZ19.h"
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
+const int rx_pin = D2; //Serial rx pin no D2
+const int tx_pin = D1; //Serial tx pin no D1
+const int pwmpin = D5; //pwm pin no D5
+MHZ19 *mhz19_uart = new MHZ19(rx_pin,tx_pin);
+MHZ19 *mhz19_pwm = new MHZ19(pwmpin);
 
+/*----------------------------------------------------------
+    MH-Z19 CO2 sensor  setup
+  ----------------------------------------------------------*/
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    mhz19_uart->begin(rx_pin, tx_pin);
+    mhz19_uart->setAutoCalibration(false);
+    // while (mhz19_uart->isWarming())
+    // {
+    //     Serial.print("MH-Z19 now warming up...  status:");
+    //     Serial.println(mhz19_uart->getStatus());
+    //     //Serial.println( mhz19_pwm->getPPM(MHZ19_POTOCOL::PWM));
+    //     delay(1000);
+    // }
 }
 
+/*----------------------------------------------------------
+    MH-Z19 CO2 sensor  loop
+  ----------------------------------------------------------*/
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+    int co2ppm = mhz19_uart->getPPM(MHZ19_POTOCOL::UART);
+    int temp = mhz19_uart->getTemperature();
 
-  // wait for a second
-  delay(1000);
+    Serial.print("co2: ");
+    Serial.println(co2ppm);
+    Serial.print("temp: ");
+    Serial.println(temp);
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+    // co2ppm = mhz19_pwm->getPPM(MHZ19_POTOCOL::PWM);
+    // Serial.print("co2: ");
+    // Serial.println(co2ppm);
+    
+    delay(5000);
 }
